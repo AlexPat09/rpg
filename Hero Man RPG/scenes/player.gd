d6cccbdac97782abe39a0823a5cdba7216ead18e
AlexPat09 <131480346+AlexPat09@users.noreply.github.com>
2023-04-22 11:54:16 +0300

    Add files via upload

---
+++ Hero Man RPG/scenes/player.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+const SPEED = 50
+
+func _physics_process(delta):
+	var directionX = Input.get_axis("left", "right")
+	if directionX:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var directionY = Input.get_axis("up", "down")
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	move_and_slide()
+
